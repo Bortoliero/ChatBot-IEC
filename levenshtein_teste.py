d386a4ab3bde9171db67d3c25883842fbfeb343c
João Pedro <joaoofficialpedro@gmail.com>
2024-06-25 19:58:33 -0300

    Adicionando codificação UTF-8

--- levenshtein_teste.py
+++ levenshtein_teste.py
@@ -1,5 +1,8 @@
 import Levenshtein
+
 str1 = "tem pão"
 str2 = "chover"
 distancia = Levenshtein.distance(str1, str2)
-print("A distância de Levenshtein entre '{}' e '{}' é: {}".format(str1, str2, distancia))
+print(
+    "\nA distância de Levenshtein entre '{}' e '{}' é: {}".format(str1, str2, distancia)
+)
