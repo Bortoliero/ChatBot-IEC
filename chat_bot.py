e071096906b9e61605123948a04e89929fa57e67
João Pedro <joaoofficialpedro@gmail.com>
2024-06-26 19:49:43 -0300

    ..

--- chat_bot.py
+++ chat_bot.py
@@ -1,4 +1,5 @@
 import Levenshtein
+import sys
 
 
 def carregar_perguntas(arquivo):
@@ -7,7 +8,6 @@ def carregar_perguntas(arquivo):
         for linha in f:
             pergunta, resposta = linha.strip().split("|")
             perguntas_respostas[pergunta.lower()] = resposta
-
     return perguntas_respostas
 
 
@@ -27,13 +27,30 @@ def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
 
 if __name__ == "__main__":
     perguntas_respostas = carregar_perguntas("perguntas.txt")
-    # limiar_distancia = int(
-    #     input("Digite o limiar de distância para considerar uma pergunta semelhante:")
-    # )
-    limiar_distancia = 20
-    while True:
-        pergunta = input("Faça uma pergunta:").lower()
-        if pergunta == "sair":
-            break
-        resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
-        print("Resposta:", resposta)
+
+    if len(sys.argv) == 2:
+        limiar_distancia = int(sys.argv[1])
+        # Modo Jenkins: processar perguntas do arquivo perguntas_automaticas.txt
+        with open("perguntas_automaticas.txt", "r", encoding="utf-8") as f:
+            perguntas = [linha.strip() for linha in f]
+        for pergunta in perguntas:
+            resposta = encontrar_resposta(
+                pergunta.lower(), perguntas_respostas, limiar_distancia
+            )
+            print(f"Pergunta: {pergunta}")
+            print(f"Resposta: {resposta}")
+    else:
+        limiar_distancia = int(
+            input(
+                "Digite o limiar de distância para considerar uma pergunta semelhante:"
+            )
+        )
+        # Modo interativo: processar perguntas do usuário
+        while True:
+            pergunta = input("Faça uma pergunta:").lower()
+            if pergunta == "sair":
+                break
+            resposta = encontrar_resposta(
+                pergunta, perguntas_respostas, limiar_distancia
+            )
+            print("Resposta:", resposta)
